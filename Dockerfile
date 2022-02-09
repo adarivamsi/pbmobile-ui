5b623db167fc952dc44ba7e7314362a5c7c869ed
Vamsi Charan Adari <adarivamsi@gmail.com>
2022-02-09 17:58:41 -0600

    added changes from app -> pb-mobile

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 #stage 1
 FROM node:16.13.2-alpine as node
 LABEL author="Vamsi Charan Adari"
-RUN mkdir -p /app
-WORKDIR /app
-COPY package.json /app
+RUN mkdir -p /pb-mobile
+WORKDIR /pb-mobile
+COPY package.json /pb-mobile
 RUN npm install
-COPY . /app
+COPY . /pb-mobile
 RUN npm run build --prod
 
 #stage 2
 FROM nginx:latest
-COPY --from=node /app/dist/pbmobile-ui /usr/share/nginx/html
+COPY --from=node /pb-mobile/dist/pbmobile-ui /usr/share/nginx/html
 EXPOSE 4200:80
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { PbMobileModule } from './pb-mobile/pb-mobile.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(PbMobileModule)
   .catch(err => console.error(err));
